4bcf6cd7bfb6a26ad183c4dc0af215f64207df06
DzoniTheNick <nikolapetrovic12@gmail.com>
2023-03-16 11:34:29 +0100

    Added rough query menu component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.scss';
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
+import Query from './components/query/query';
 
 const App = () => {
 
   return (
     <div className="app">
       <Header />
+      <Query />
       <Footer />
     </div>
   );
